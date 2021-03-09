4b084c0f7bf01889e26a16d1eea11a948725aa87
Ygor <ygor_sy@hotmail.com>
2021-03-09 02:48:13 -0300

    teste

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import ThemeManager from '@/theme/ThemeManager';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
-    <Head>
+    <Head                 >
       <title>Portifolio - Ygor Yamanaca</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
